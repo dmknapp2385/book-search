8e842f8275ee692674894137e9733c683295a0ed
dmknapp2385 <dmknapp2385@gmail.com>
2022-03-30 15:45:51 -0700

    app.js file updated to include apollo/client and connection to the apollo server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+//create link to the graphql server using proxy in package.json
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//add authorization to header for context in resolvers
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+//establish a new connection to the apollo server and new memory cache for queries
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
